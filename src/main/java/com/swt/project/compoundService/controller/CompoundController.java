daa7075b01ac08216395ec182d97a947e6a22d13
Lukas Rottinghaus <Lukas.Rottinghaus@web.de>
2022-11-05 02:31:30 +0100

    test for jenkins server

--- src/main/java/com/swt/project/compoundService/controller/CompoundController.java
+++ src/main/java/com/swt/project/compoundService/controller/CompoundController.java
@@ -105,7 +105,7 @@ public class CompoundController {
     }
 
     /**
-     * Post method to save data to server
+     * Post method to save data to the server
      *
      * @param compoundInterest - parameter from type compoundInterest
      * @return ResponseEntity
